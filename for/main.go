e02b7ac20338047a090dea5b78934ef647ebf236
Vinicius Freitas <viniciuslf@gmail.com>
2020-03-04 16:07:27 -0300

    for final

--- for/main.go
+++ for/main.go
@@ -1,5 +1,34 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	for i := 1; i <= 10; i++ {
+		// fmt.Println("i =", i)
+	}
+
+	valor := 0
+	teste := true
+
+	for teste {
+		valor++
+		if valor%5 == 0 {
+			teste = false
+		}
+		fmt.Println("O número é:", valor)
+	}
+
+	for {
+		valor--
+		fmt.Println("O número é:", valor)
+		if valor == 0 {
+			break
+		}
+	}
+
+	texto := "Eu gosto de escrever promgramas em Go"
+	for indice, letra := range texto {
+		fmt.Printf("Texto[%d] = %q\r\n", indice, letra)
+	}
 }
